e7c7ae3ba2680f935fc85772d0301b57a00c0fa6
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2021-09-15 10:35:10 -0500

    ignore file

---
+++ app.py
@@ -0,0 +1,10 @@
+'''
+IQR
+Z-score (SD of 2, 3, or 4 depending on population size)
+From  sci-kit learn
+    One-Class Classification (OCC), since we'll be doing this for one feature at a time, this has a good likelihood of providing a useful result; may have to deviate if we want ID outliers by LOB, chassis, brand or family - but that's on down the road
+    LocalOutlierFactor (LOF) class
+    Isolation Forest
+    Minimum Covariance Determinant
+    One-Class SVM
+'''
